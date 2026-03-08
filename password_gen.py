01e7e261e81cbe54ccaf52f2e1f9e9b3c7b30b0d
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-08 20:55:57 +0700

    Create password_gen.py

---
+++ password_gen.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*"
+    return ''.join(random.choice(chars) for i in range(length))
+
+print("Mật khẩu mới của bạn là:", generate_password(12))
