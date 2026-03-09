4efbe7348bce9a6f3c90ff19eff6ee6f9d75a582
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-09 14:08:21 +0700

    Update img_to_pdf.py

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -16,3 +16,9 @@ def convert_img_pdf(img_path, pdf_path):
     # Bước này rất quan trọng vì PDF thường yêu cầu định dạng RGB, 
     # và nó giúp loại bỏ kênh Alpha (độ trong suốt) nếu ảnh gốc là PNG
     img_converted = image.convert('RGB')
+# Lưu ảnh đã chuyển đổi dưới định dạng PDF vào đường dẫn pdf_path
+    # Thư viện Pillow sẽ tự nhận diện định dạng dựa trên đuôi file .pdf
+    img_converted.save(pdf_path)
+    
+    # Thông báo ra màn hình sau khi hoàn tất
+    print("Đã chuyển đổi xong!")
