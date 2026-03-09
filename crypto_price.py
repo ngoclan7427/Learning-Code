132538e8cf08487a090a61a153ce9f4bdd0063f7
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-09 14:00:30 +0700

    Update crypto_price.py

--- crypto_price.py
+++ crypto_price.py
@@ -12,3 +12,9 @@ import requests  # Import thư viện requests để gửi yêu cầu HTTP đế
 def get_btc_price():
     # URL của API CoinDesk cung cấp giá Bitcoin thời gian thực (định dạng JSON)
     url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+ # Gửi yêu cầu GET đến URL và chuyển đổi nội dung phản hồi (JSON) thành một Dictionary trong Python
+    response = requests.get(url).json()
+    
+    # Truy cập vào các khóa trong Dictionary để lấy giá trị 'rate' (giá Bitcoin) của đồng USD
+    # Cấu trúc JSON: response -> 'bpi' -> 'USD' -> 'rate'
+    price = response['bpi']['USD']['rate']
