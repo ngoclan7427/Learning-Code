8124e5db07c3b61bd5cea54cab2b253466831911
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-10 09:34:51 +0700

    Update organize_folder.py

--- organize_folder.py
+++ organize_folder.py
@@ -23,3 +23,6 @@ for file in files:
     extension = extension[1:]
     if os.path.exists(path+'/'+extension):
         shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+else:
+        os.makedirs(path+'/'+extension)
+        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
