a2fcd7713e5e3eda4735a9f91d1af1b438b67eaf
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-10 09:34:38 +0700

    Update organize_folder.py

--- organize_folder.py
+++ organize_folder.py
@@ -18,3 +18,8 @@ import shutil
 # Đoạn code này tự động phân loại và di chuyển các tệp tin vào thư mục riêng dựa trên phần mở rộng (đuôi file) của chúng
 path = "/Users/YourName/Downloads"
 files = os.listdir(path)
+for file in files:
+    filename, extension = os.path.splitext(file)
+    extension = extension[1:]
+    if os.path.exists(path+'/'+extension):
+        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
