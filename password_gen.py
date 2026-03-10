7d76478f0871fb3d59dc8c3938d651585e275a3c
ngoclan7427 <nguyenngoclan7427@gmail.com>
2026-03-10 19:28:24 +0700

    Update password_gen.py

--- password_gen.py
+++ password_gen.py
@@ -13,3 +13,9 @@ def generate_password(length):
 print("Mật khẩu mới của bạn là:", generate_password(12))
 import string  # Nhập thư viện chứa các chuỗi ký tự mẫu (chữ cái, chữ số)
 import random  # Nhập thư viện để thực hiện việc chọn ngẫu nhiên
+def generate_password(length):
+    # Tạo một chuỗi chứa tất cả các ký tự cho phép:
+    # string.ascii_letters: bao gồm chữ hoa (A-Z) và chữ thường (a-z)
+    # string.digits: bao gồm các chữ số từ 0-9
+    # "!@#$%^&*": các ký tự đặc biệt tự định nghĩa thêm
+    chars = string.ascii_letters + string.digits + "!@#$%^&*"
